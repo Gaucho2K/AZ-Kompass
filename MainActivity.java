1b41f1f641d245b0a102d25d50141fa8139bb40d
Gaucho2K <heindel2k@gmail.com>
2026-09-22 06:10:59 +0200

    Aktualisieren von MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -10,6 +10,11 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
 public class MainActivity extends Activity {
     private WebView webView;
 
@@ -38,7 +43,33 @@ public class MainActivity extends Activity {
         settings.setTextZoom(100);
 
         setContentView(webView);
-        webView.loadUrl("file:///android_asset/index.html");
+        loadLocalIndexUtf8();
+    }
+
+    private void loadLocalIndexUtf8() {
+        try (InputStream inputStream = getAssets().open("index.html")) {
+            byte[] bytes = readAllBytes(inputStream);
+            String html = new String(bytes, StandardCharsets.UTF_8);
+            webView.loadDataWithBaseURL(
+                    "file:///android_asset/",
+                    html,
+                    "text/html",
+                    "UTF-8",
+                    null
+            );
+        } catch (IOException e) {
+            webView.loadUrl("file:///android_asset/index.html");
+        }
+    }
+
+    private byte[] readAllBytes(InputStream inputStream) throws IOException {
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        byte[] data = new byte[4096];
+        int nRead;
+        while ((nRead = inputStream.read(data, 0, data.length)) != -1) {
+            buffer.write(data, 0, nRead);
+        }
+        return buffer.toByteArray();
     }
 
     @Override
